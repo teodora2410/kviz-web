b9af5ce5c2c2e133651fd1a619f98b5890302d51
teodora2410 <74426233+teodora2410@users.noreply.github.com>
2025-10-02 22:39:27 +0200

    auto maper

---
+++ web_api/IMappingHelper/MappingHelper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Kviz.Domain;
+using Kviz.Service.DataTransferObjects;
+
+namespace Kviz.Service.IMappingHelper
+{
+    public class MappingHelper : Profile
+    {
+        public MappingHelper()
+        {
+            CreateMap<User, UserDto>().ReverseMap();
+            CreateMap<Quiz, QuizDto>().ReverseMap();
+            CreateMap<Question, QuestionDto>().ReverseMap();
+            CreateMap<Answer, AnswerDto>().ReverseMap();
+            CreateMap<Result, ResultDto>().ReverseMap();
+            CreateMap<ResultAnswer, ResultAnswerDto>().ReverseMap();
+        }
+    }
+}
