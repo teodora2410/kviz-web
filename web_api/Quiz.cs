ce422b445ca3e5387d1f8e49f66d024520db1569
teodora2410 <74426233+teodora2410@users.noreply.github.com>
2025-10-03 22:40:23 +0200

    kviz, pitanje model

---
+++ web_api/Quiz.cs
@@ -0,0 +1,34 @@
+﻿namespace Kviz.Domain
+{
+    public class Quiz
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Categories { get; set; }
+        public int TimeLimit { get; set; }
+        public string Level { get; set; }
+        public bool IsDeleted { get; set; }
+        public int OriginalQuizId { get; set; }
+
+        public Quiz()
+        {
+            Title = string.Empty;
+            Description = string.Empty;
+            Categories = string.Empty;
+            Level = string.Empty;
+        }
+
+        public Quiz(int id, string title, string description, string categories, int timeLimit, string level, bool isDeleted, int originalQuizId)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Categories = categories;
+            TimeLimit = timeLimit;
+            Level = level;
+            IsDeleted = isDeleted;
+            OriginalQuizId = originalQuizId;
+        }
+    }
+}
