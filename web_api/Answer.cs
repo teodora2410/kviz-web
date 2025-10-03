329497e4c9fb365cc9c341f3729ca4e1b74789c5
teodora2410 <74426233+teodora2410@users.noreply.github.com>
2025-10-03 22:40:32 +0200

    odgovor i rezultat model

---
+++ web_api/Answer.cs
@@ -0,0 +1,25 @@
+﻿namespace Kviz.Domain
+{
+    public class Answer
+    {
+        public int Id { get; set; }
+        public int QuizId { get; set; }
+        public int QuestionId { get; set; }
+        public string Text { get; set; }
+        public bool Correct { get; set; }
+
+        public Answer()
+        {
+            Text = string.Empty;
+        }
+
+        public Answer(int id, int quizId, int questionId, string text, bool correct)
+        {
+            Id = id;
+            QuizId = quizId;
+            QuestionId = questionId;
+            Text = text;
+            Correct = correct;
+        }
+    }
+}
---
+++ web_api/Result.cs
@@ -0,0 +1,25 @@
+﻿namespace Kviz.Domain
+{
+    public class Result
+    {
+        public int Id { get; set; }
+        public int QuizId { get; set; }
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public DateTime SolvedAt { get; set; }
+
+        public Result()
+        {
+            Username = string.Empty;
+        }
+
+        public Result(int id, int quizId, int userId, string username, DateTime solvedAt)
+        {
+            Id = id;
+            QuizId = quizId;
+            UserId = userId;
+            Username = username;
+            SolvedAt = solvedAt;
+        }
+    }
+}
