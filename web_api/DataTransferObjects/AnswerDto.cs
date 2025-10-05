ff4b3e3d8557f05a6e800d3498a7d54960e6f9db
teodora2410 <74426233+teodora2410@users.noreply.github.com>
2025-10-05 22:42:47 +0200

    dto za servis

---
+++ web_api/DataTransferObjects/AnswerDto.cs
@@ -0,0 +1,25 @@
+﻿namespace Kviz.Service.DataTransferObjects
+{
+    public class AnswerDto
+    {
+        public int Id { get; set; }
+        public int QuizId { get; set; }
+        public int QuestionId { get; set; }
+        public string Text { get; set; }
+        public bool Correct { get; set; }
+
+        public AnswerDto()
+        {
+            Text = string.Empty;
+        }
+
+        public AnswerDto(int id, int quizId, int questionId, string text, bool correct)
+        {
+            Id = id;
+            QuizId = quizId;
+            QuestionId = questionId;
+            Text = text;
+            Correct = correct;
+        }
+    }
+}
---
+++ web_api/DataTransferObjects/LoginDto.cs
@@ -0,0 +1,20 @@
+﻿namespace Kviz.Service.DataTransferObjects
+{
+    public class LoginDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public LoginDto()
+        {
+            Username = string.Empty;
+            Password = string.Empty;
+        }
+
+        public LoginDto(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+    }
+}
