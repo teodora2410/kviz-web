d94a95d02e06f69c14836a441d4c8f292e1698d2
teodora2410 <74426233+teodora2410@users.noreply.github.com>
2025-10-08 22:42:02 +0200

    auth servis

---
+++ web_api/IntegrityService.cs
@@ -0,0 +1,32 @@
+﻿using Kviz.Service.DataTransferObjects;
+using Kviz.Service.IServices;
+using Microsoft.IdentityModel.Tokens;
+using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Kviz.Service.Services
+{
+    public class IntegrityService : IIntegrityService
+    {
+        public async Task<string?> GenerateIntegrityToken(string key, string issuer, string audience, int iex, UserDto user)
+        {
+            try
+            {
+                var credentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)), SecurityAlgorithms.HmacSha256);
+                var token = new JwtSecurityToken(issuer: issuer, audience: audience,
+                    claims: [new Claim("id", user.Id.ToString()), new Claim("email", user.Email), new Claim("role", user.IsAdmin ? "Admin" : "User")],
+                    expires: DateTime.UtcNow.AddHours(iex), signingCredentials: credentials
+                );
+
+                return await Task.FromResult(new JwtSecurityTokenHandler().WriteToken(token));
+            }
+            catch (Exception e)
+            {
+                Trace.WriteLine(e.Message);
+                return null;
+            }
+        }
+    }
+}
